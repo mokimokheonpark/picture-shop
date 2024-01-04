7b12028361f45b1cee2bb7df85c6e139c554775c
Mokheon Park <mokimokheonpark@gmail.com>
2024-01-04 15:49:35 -0500

    created Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div className="App">HELLO</div>;
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default App;
