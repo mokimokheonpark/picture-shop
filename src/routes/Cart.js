3e78579de6ef64dc7d3380d6b16cef7f1cd7197e
Mokheon Park <mokimokheonpark@gmail.com>
2024-01-08 17:17:26 -0500

    did data binding on Cart using Redux

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,24 +1,29 @@
+import { useSelector } from "react-redux";
 import { Table } from "react-bootstrap";
 
 function Cart() {
+  const state = useSelector((state) => {
+    return state;
+  });
+
   return (
     <Table>
       <thead>
         <tr>
-          <th>#</th>
+          <th>Order</th>
           <th>Title</th>
           <th>Quantity</th>
-          <th>Edit</th>
+          <th>User</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Picture 0</td>
-          <td>3</td>
-          <td>???</td>
+      {state.cart.map((_, i) => (
+        <tr key={i}>
+          <td>{i + 1}</td>
+          <td>{state.cart[i].title}</td>
+          <td>{state.cart[i].quantity}</td>
+          <td>{state.cart[i].totalPrice}</td>
         </tr>
-      </tbody>
+      ))}
     </Table>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const cart = createSlice({
+  name: "cart",
+  initialState: [
+    { title: "Picture 0", quantity: 2, totalPrice: 200 },
+    { title: "Picture 2", quantity: 1, totalPrice: 299 },
+  ],
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    cart: cart.reducer,
+  },
 });
